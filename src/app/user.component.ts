957861a18ba66b1dee5f11b45f039686b556e7f7
Stephen Shelton <stephenshelton777@gmail.com>
2017-07-20 23:11:58 -0700

    add component fix

---
+++ src/app/user.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  template: `
+    <input type=“text” (input)="onUserInput($event)" [value]="name">
+    <p>Hello! I am {{ name }}</p>
+    <p>I'm the user component</p>
+  `
+})
+export class UserComponent {
+  name = 'Stephen';
+
+  onUserInput(event) {
+    this.name = event.target.value;
+  }
+}
