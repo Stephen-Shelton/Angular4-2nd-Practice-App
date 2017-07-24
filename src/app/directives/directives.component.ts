8435cd530de2c502c557cc15f15b601f82386dc2
stephen-shelton <stephenshelton777@gmail.com>
2017-07-24 10:56:17 -0700

    practice with directives

---
+++ src/app/directives/directives.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directives',
+  templateUrl: './directives.component.html',
+  styleUrls: ['./directives.component.css']
+})
+export class DirectivesComponent implements OnInit {
+  items = [1, 2, 3];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
